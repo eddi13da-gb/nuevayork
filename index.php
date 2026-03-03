2669351bbcb18f31d2c10ab554db382a73c87b5a
Eddi Daza <eddidazagbmediagroup@gmail.com>
2026-03-03 09:51:11 -0500

    segundo comict

--- index.php
+++ index.php
@@ -9,11 +9,19 @@ class nombres {
   }
 public function saludar1(){
 if ($this->edad >=18) {
-    echo "Hola " . $this->nombre . " tu edad es " . $this->edad;
+   return "Hola " . $this->nombre . " tu edad es " . $this->edad;
 } else {
-  echo"meeh";
+  return "meeh";
 }
   }
 }
 $nombre= new nombres("Eddi", 80);
 $nombre->saludar1();
+
+echo $nombre->saludar1();
+$nombre= new nombres("Eddi", 80);
+echo "<br><br>";
+echo $nombre->saludar1();
+$nombre= new nombres("luis", 80);
+echo "<br><br>";
+echo $nombre->saludar1();
